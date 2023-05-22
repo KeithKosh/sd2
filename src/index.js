bbb8e1ffe52c15cfc5f3c793cdd91c140b172075
Keith Koshman <keith@keithkosh.ca>
2023-05-21 22:23:37 -0600

    Add initial rect tile render, aspect ratio

--- src/index.js
+++ src/index.js
@@ -1,18 +1,82 @@
 import settings from "./settings.js";
 
-let canvas; // necessary?
+// TODO: these should be moved somewhere better probably
+let canvas;
+let context;
+
+let canvasWidth;
+let canvasHeight;
 
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
   canvas = document.createElement("canvas");
   canvas.id = "gameCanvas";
-  canvas.width = 500;
-  canvas.height = 500;
 
-  let domContext = canvas.getContext('2d');
-  domContext.scale(settings.pixelScale, settings.pixelScale);
-  domContext.imageSmoothingEnabled = false;
+  // TODO, not sure if best place for this...
+  canvasWidth = settings.SCREEN_WIDTH * settings.TILE_SIZE * settings.PIXEL_SCALE;
+  canvasHeight = settings.SCREEN_HEIGHT * settings.TILE_SIZE * settings.PIXEL_SCALE;
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
 
+  context = canvas.getContext('2d');
+  context.scale(settings.PIXEL_SCALE, settings.PIXEL_SCALE);
+  context.imageSmoothingEnabled = false;
+
+  // TODO, these might end up moving/changing...
+  document.body.style.height = '100%';
+  document.body.style.overflow = 'hidden';
   document.body.appendChild(canvas);
+
+  gameLoop();
+}
+
+function gameLoop() {
+  // TODO: need to cap this at 60fps
+
+  clearCanvas();
+  drawBG();
+  drawTiles(); // TODO: this will be "lower layer" tiles as a param
+  // TODO: read input
+  // TODO: update movement vars, camera, etc.
+  // TODO: draw sprites
+  // TODO: draw FG tiles (i.e. drawTiles(false) for "higher layer" tiles)
+  // TODO: any screen effects? distortion, lighting, etc...
+  // TODO: draw UI
+
+  //requestAnimationFrame(gameLoop);
+}
+
+function clearCanvas() {
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+function drawBG() {
+  // TODO: this may be replaced with an image or even with pure tiles instead?
+  context.fillStyle = "#002080";
+  context.fillRect(0, 0, 500, 500);
+}
+
+function drawTiles() {
+  // TODO: draw to a temp canvas before we copy to screen as LAST STEP
+
+  // TODO, this will obviously be replaced with png sprite(s)...
+  let drawTile = (x, y) => {
+    context.fillStyle = '#505050';
+    context.fillRect(x, y, settings.TILE_SIZE, settings.TILE_SIZE);
+    context.fillStyle = '#aaa';
+    context.fillRect(x + 1, y + 1, settings.TILE_SIZE - 2, settings.TILE_SIZE - 2);
+  }
+
+  // TODO: replace with loaded map data(probably array or string parse)
+  // just a border for now...
+  for (let y = 0; y < settings.SCREEN_HEIGHT; y++) {
+    for (let x = 0; x < settings.SCREEN_WIDTH; x++) {
+      // only draw the full top and bottom rows, and the first/last column otherwise
+      if ((y == 0 || y == settings.SCREEN_HEIGHT - 1) ||
+        (x == 0 || x == settings.SCREEN_WIDTH - 1)) {
+        drawTile(x * settings.TILE_SIZE, y * settings.TILE_SIZE);
+      }
+    }
+  }
 }
--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,16 @@
 const settings = {
   /* default amount to scale up pixels by. this will likely change when we put in
      auto-scaling based on screen size...? */
-  pixelScale: 4,
+  PIXEL_SCALE: 4,
+
+  // size of a tile's width and height (before any scaling)
+  TILE_SIZE: 15, 
+
+  // width of screen in TILES.
+  SCREEN_WIDTH: 25,
+
+  // height of screen in TILES.
+  SCREEN_HEIGHT: 13,
 }
 
 export default settings;
